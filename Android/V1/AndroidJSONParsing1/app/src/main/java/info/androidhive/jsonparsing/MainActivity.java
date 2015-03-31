f6bd40a48e32cb69fba9cc3afae12b8f5e92d737
x00090749 <x00090749@ittd.ie>
2015-03-31 13:27:45 +0100

    Android V1 update

--- Android/V1/AndroidJSONParsing1/app/src/main/java/info/androidhive/jsonparsing/MainActivity.java
+++ Android/V1/AndroidJSONParsing1/app/src/main/java/info/androidhive/jsonparsing/MainActivity.java
@@ -31,16 +31,6 @@ public class MainActivity extends ListActivity {
     private static String url = "http://uefaservicev3.azurewebsites.net/api/countries/";
 
 	// JSON Node names
-	//private static final String TAG_CONTACTS = "contacts";
-	//private static final String TAG_ID = "id";
-	/*private static final String TAG_NAME = "name";
-	private static final String TAG_EMAIL = "email";
-	private static final String TAG_ADDRESS = "address";
-	private static final String TAG_GENDER = "gender";
-	private static final String TAG_PHONE = "phone";
-	private static final String TAG_PHONE_MOBILE = "mobile";
-	private static final String TAG_PHONE_HOME = "home";
-	private static final String TAG_PHONE_OFFICE = "office";*/
 
     private static final String TAG_COUNTRIES = "Country"; //countries
     private static final String TAG_ID = "Id";
@@ -124,23 +114,17 @@ public class MainActivity extends ListActivity {
 					JSONObject jsonObj = new JSONObject(jsonStr);
 					
 					// Getting JSON Array node
-					countries = jsonObj.getJSONArray(TAG_COUNTRIES);
+					countries = jsonObj.getJSONArray("Country"); //TAG_COUNTRIES
 
-					// looping through All Contacts
+					// looping through All Countries
 					for (int i = 0; i < countries.length(); i++) {
 						JSONObject c = countries.getJSONObject(i);
 						
-						String Id = c.getString(TAG_ID);
-						String CountryName = c.getString(TAG_COUNTRYNAME);
-						String CountryCode = c.getString(TAG_COUNTRYCODE); //email
-						String Position = c.getString(TAG_POSITION); //address
-						//String gender = c.getString(TAG_GENDER);
+						String Id = c.getString(TAG_ID); //Id
+						String CountryName = c.getString(TAG_COUNTRYNAME); //"CountryName"
+						String CountryCode = c.getString(TAG_COUNTRYCODE); //"CountryCode
+						String Position = c.getString(TAG_POSITION); //"Position"
 
-						// Phone node is JSON Object
-						/*JSONObject phone = c.getJSONObject(TAG_PHONE);
-						String mobile = phone.getString(TAG_PHONE_MOBILE);
-						String home = phone.getString(TAG_PHONE_HOME);
-						String office = phone.getString(TAG_PHONE_OFFICE);*/
 
 						// tmp hashmap for single contact
 						HashMap<String, String> country = new HashMap<String, String>();
